de9ec04fd3c797986f66e2a6fd5c501a7be0dfd8
Chris Bandy <bandy.chris@gmail.com>
2017-04-09 05:28:48 +0000

    Install and configure Ruby

---
+++ ruby.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+. share/functions.sh
+
+set -eu
+
+local_file "$HOME/.gemrc" "files/ruby/gemrc"
+local_file "$HOME/.rspec" "files/ruby/rspec"
+
+silent command -v chruby || {
+	chruby_checksum='7220a96e355b8a613929881c091ca85ec809153988d7d691299e0a16806b42fd'
+	chruby_github='github.com/postmodern/chruby'
+	chruby_version='0.3.9'
+
+	remote_file "/tmp/chruby-${chruby_version}.tgz" "https://${chruby_github}/archive/v${chruby_version}.tar.gz" "$chruby_checksum"
+	tar --file  "/tmp/chruby-${chruby_version}.tgz" --extract --directory '/tmp'
+	( cd "/tmp/chruby-${chruby_version}" && sudo make install )
+}
+
+grep --silent 'chruby/chruby' "$HOME/.bashrc" || echo >> "$HOME/.bashrc" 'source /usr/local/share/chruby/chruby.sh'
+grep --silent 'chruby/auto'   "$HOME/.bashrc" || echo >> "$HOME/.bashrc" 'source /usr/local/share/chruby/auto.sh' 
+
+silent command -v ruby-install || {
+	install_checksum='b3adf199f8cd8f8d4a6176ab605db9ddd8521df8dbb2212f58f7b8273ed85e73'
+	install_github='github.com/postmodern/ruby-install'
+	install_version='0.6.1'
+
+	remote_file "/tmp/ruby-install-${install_version}.tgz" "https://${install_github}/archive/v${install_version}.tar.gz" "$install_checksum"
+	tar --file  "/tmp/ruby-install-${install_version}.tgz" --extract --directory '/tmp'
+	( cd "/tmp/ruby-install-${install_version}" && sudo make install )
+}
+
+ruby-install --no-reinstall ruby -- --disable-install-doc
