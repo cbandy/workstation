725682a529a827b94e6534ad6f45fe9f958eea2d
Chris Bandy <bandy.chris@gmail.com>
2019-08-20 19:17:13 -0500

    Update to Go 1.12.9

--- go.sh
+++ go.sh
@@ -4,10 +4,10 @@
 
 set -eu
 
-go_version='1.12.4'
+go_version='1.12.9'
 
 test "go${go_version}" = "$( a=($(silent command -v go && go version)); echo "${a[2]-}" )" || {
-	go_checksum='d7d1f1f88ddfe55840712dc1747f37a790cbcaa448f6c9cf51bbe10aa65442f5'
+	go_checksum='ac2a6efcc1f5ec8bdc0db0a988bb1d301d64b6d61b7e8d9e42f662fbb75a2b9b'
 	go_machine='amd64'
 
 	remote_file "/tmp/go-${go_version}.tgz" "https://storage.googleapis.com/golang/go${go_version}.linux-${go_machine}.tar.gz" "$go_checksum"
