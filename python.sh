d122e08bdcd7b6bd021bbc460456eefd77d80b42
Chris Bandy <bandy.chris@gmail.com>
2019-09-11 11:49:00 -0500

    Install Python using Homebrew on macOS

--- python.sh
+++ python.sh
@@ -4,8 +4,17 @@
 
 set -eu
 
-export PATH="$PATH:$HOME/.local/bin"
+export LANG='en_US.UTF-8'
+export PATH="$HOME/.local/bin:$PATH"
+export PYTHONUSERBASE="$HOME/.local"
 
-silent command -v pip3 || install_packages python3-pip python3-setuptools python3-wheel
+if ! silent command -v pip3; then
+	if [ "${OS[distribution]}" = 'macOS' ]; then
+		install_packages python
+	else
+		install_packages python3-pip python3-setuptools python3-wheel
+	fi
+fi
+
 silent command -v pygmentize || pip3 install --user pygments
 pygmentize -L styles | grep --silent base16 || pip3 install --user pygments-base16
