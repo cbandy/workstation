72a5215f45a31269463969b01f1a8b39d0b0ee59
Chris Bandy <bandy.chris@gmail.com>
2019-09-10 20:11:05 -0500

    Install Docker using Homebrew on macOS

--- docker.sh
+++ docker.sh
@@ -4,19 +4,25 @@
 
 set -eu
 
-export PATH="$PATH:$HOME/.local/bin"
+export PATH="$HOME/.local/bin:$PATH"
+
+if [ "${OS[distribution]}" = 'macOS' ]; then
+	silent brew cask list 'docker' || install_cask 'docker'
+	silent command -v 'dive' || install_packages 'dive'
+	exit
+fi
 
 if ! silent command -v 'docker'; then
+	build="${OS[kernel],,}/${OS[distribution],,}"
 	checksum='1500c1f56fa9e26b9b8f42452a553675796ade0807cdce11975eb98170b3a570'
-	repository="download.docker.com/${OS[kernel],,}/${OS[distribution],,}"
 	key='0EBFCD88'
 
 	if [ -z "$(apt-key list "$key")" ]; then
-		remote_file      "/tmp/${key}.asc" "https://${repository}/gpg" "$checksum"
+		remote_file      "/tmp/${key}.asc" "https://download.docker.com/${build}/gpg" "$checksum"
 		sudo apt-key add "/tmp/${key}.asc"
 	fi
 
-	install_package_repository "deb [arch=${OS[machine]/x86_/amd}] https://${repository} ${OS[codename]} stable"
+	install_package_repository "deb [arch=${OS[machine]/x86_/amd}] https://download.docker.com/${build} ${OS[codename]} stable"
 	install_packages 'docker-ce'
 fi
 
@@ -32,15 +38,17 @@ if [ -e "$docker_socket" ]; then
 	fi
 fi
 
-checksum='bee6460f96339d5d978bb63d17943f773e1a140242dfa6c941d5e020a302c91b'
-project='github.com/docker/compose'
 version='1.24.0'
 
 if [ "${version}," != "$( read -ra array <<< "$(maybe docker-compose --version)"; echo "${array[2]-}" )" ]
 then
+	build="${OS[kernel]}-${OS[machine]}"
+	checksum='bee6460f96339d5d978bb63d17943f773e1a140242dfa6c941d5e020a302c91b'
+
 	remote_file "/tmp/docker-compose-${version}" \
-		"https://${project}/releases/download/${version}/docker-compose-${OS[kernel]}-${OS[machine]}" \
+		"https://github.com/docker/compose/releases/download/${version}/docker-compose-${build}" \
 		"$checksum"
 
-	install --no-target-directory "/tmp/docker-compose-${version}" "$HOME/.local/bin/docker-compose"
+	mkdir -p "$HOME/.local/bin"
+	install_file "$HOME/.local/bin/docker-compose" "/tmp/docker-compose-${version}"
 fi
