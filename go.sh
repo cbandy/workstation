e3b856d16e8fdbc236971889343a88be67edfbdd
Chris Bandy <bandy.chris@gmail.com>
2019-09-10 21:16:12 -0500

    Include checksum for Go on macOS

--- go.sh
+++ go.sh
@@ -4,15 +4,20 @@
 
 set -eu
 
-export PATH="$PATH:$HOME/.local/go/bin"
+export PATH="$HOME/.local/go/bin:$PATH"
 
-checksum='ac2a6efcc1f5ec8bdc0db0a988bb1d301d64b6d61b7e8d9e42f662fbb75a2b9b'
 version='1.12.9'
 
 if [ "go${version}" != "$( read -ra array <<< "$(maybe go version)"; echo "${array[2]-}" )" ]
 then
+	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
+	case "$build" in
+		'darwin-amd64') checksum='4f189102b15de0be1852d03a764acb7ac5ea2c67672a6ad3a340bd18d0e04bb4' ;;
+		'linux-amd64')  checksum='ac2a6efcc1f5ec8bdc0db0a988bb1d301d64b6d61b7e8d9e42f662fbb75a2b9b' ;;
+	esac
+
 	remote_file "/tmp/go-${version}.tgz" \
-		"https://dl.google.com/go/go${version}.${OS[kernel],,}-${OS[machine]/x86_/amd}.tar.gz" \
+		"https://dl.google.com/go/go${version}.${build}.tar.gz" \
 		"$checksum"
 
 	tar --file "/tmp/go-${version}.tgz" --extract --directory '/tmp'
