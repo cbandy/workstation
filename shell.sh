4697b93835a44bcb2f68d7cd254613cd01906770
Chris Bandy <bandy.chris@gmail.com>
2019-09-10 19:22:56 -0500

    Install shellcheck using Homebrew on macOS

--- shell.sh
+++ shell.sh
@@ -4,6 +4,8 @@
 
 set -eu
 
+export PATH="$HOME/.local/bin:$PATH"
+
 mkdir -p "$HOME/.config" "$HOME/.local/bin"
 
 local_file "$HOME/.config/interactive" 'files/shell/interactive'
@@ -16,7 +18,12 @@ uninstall_packages 'command-not-found' 'command-not-found-data'
 
 packages=()
 
-silent command -v ag   || packages+=('silversearcher-ag')
+if [ "${OS[distribution]}" = 'macOS' ]; then
+	silent command -v ag || packages+=('the_silver_searcher')
+else
+	silent command -v ag || packages+=('silversearcher-ag')
+fi
+
 silent command -v curl || packages+=('curl')
 silent command -v htop || packages+=('htop')
 silent command -v jq   || packages+=('jq')
@@ -26,15 +33,21 @@ silent command -v zip  || packages+=('zip')
 
 [ "${#packages[@]}" -eq 0 ] || install_packages "${packages[@]}"
 
-checksum='84e06bee3c8b8c25f46906350fb32708f4b661636c04e55bd19cdd1071265112d84906055372149678d37f09a1667019488c62a0561b81fe6a6b45ad4fae4ac0'
 version='0.7.0'
 
 if [ "version: ${version}" != "$(maybe shellcheck --version | grep 'version:')" ]
 then
-	remote_file "/tmp/shellcheck-${version}.txz" \
-		"https://shellcheck.storage.googleapis.com/shellcheck-v${version}.${OS[kernel],,}.${OS[machine]}.tar.xz" \
-		"$checksum"
+	if [ "${OS[distribution]}" = 'macOS' ]; then
+		install_packages 'shellcheck'
+	else
+		build="${OS[kernel],,}.${OS[machine]}"
+		checksum='84e06bee3c8b8c25f46906350fb32708f4b661636c04e55bd19cdd1071265112d84906055372149678d37f09a1667019488c62a0561b81fe6a6b45ad4fae4ac0'
 
-	tar --file "/tmp/shellcheck-${version}.txz" --extract --directory '/tmp'
-	install --no-target-directory "/tmp/shellcheck-v${version}/shellcheck" "$HOME/.local/bin/shellcheck"
+		remote_file "/tmp/shellcheck-${version}.txz" \
+			"https://shellcheck.storage.googleapis.com/shellcheck-v${version}.${build}.tar.xz" \
+			"$checksum"
+
+		tar --file "/tmp/shellcheck-${version}.txz" --extract --directory '/tmp'
+		install_file "$HOME/.local/bin/shellcheck" "/tmp/shellcheck-v${version}/shellcheck"
+	fi
 fi
