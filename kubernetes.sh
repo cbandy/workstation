29a5a3a5a3aa9244b92db0b81ed830301a65a3f4
Chris Bandy <bandy.chris@gmail.com>
2020-04-04 11:37:01 -0500

    Update to k3d v1.7.0

--- kubernetes.sh
+++ kubernetes.sh
@@ -4,14 +4,14 @@ set -eu
 
 export PATH="$HOME/.local/bin:$PATH"
 
-version='1.3.1'
+version='1.7.0'
 
 if [ "v${version}" != "$( read -ra array <<< "$(maybe k3d --version)"; echo "${array[2]-}" )" ]
 then
 	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
 	case "$build" in
-		'darwin-amd64') checksum='846b4f47ef7cb794dd2ecdedda66cfe72bf0a7345c51f16afe3f5e7c9b3e5ad9' ;;
-		'linux-amd64')  checksum='52b127b76ef76652adf94e8310c85270ae24831ca222bb082d5ced0e80380916' ;;
+		'darwin-amd64') checksum='c24add17c29c95c239efba277977cb1ecdfb77255d57ade004059cb04bf28250' ;;
+		'linux-amd64')  checksum='da9ff31bcf4377fadfb065f4998d347f19de1168a5a553ce2c23b763ee1f6098' ;;
 	esac
 
 	remote_file "/tmp/k3d-${version}" \
