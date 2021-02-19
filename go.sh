83a27d508f6e48b453e14a3817f0370102a53672
Chris Bandy <bandy.chris@gmail.com>
2021-02-18 21:58:33 -0600

    Update to Go 1.16

--- go.sh
+++ go.sh
@@ -4,14 +4,14 @@ set -eu
 
 export PATH="$HOME/.local/go/bin:$PATH"
 
-version='1.15'
+version='1.16'
 
 if [ "go${version}" != "$( read -ra array <<< "$(maybe go version)"; echo "${array[2]-}" )" ]
 then
 	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
 	case "$build" in
-		'darwin-amd64') checksum='8a5fb9c8587854a84957a79b9616070b63d8842d4001c3c7d86f261cd7b5ffb6' ;;
-		'linux-amd64')  checksum='2d75848ac606061efe52a8068d0e647b35ce487a15bb52272c427df485193602' ;;
+		'darwin-amd64') checksum='6000a9522975d116bf76044967d7e69e04e982e9625330d9a539a8b45395f9a8' ;;
+		'linux-amd64')  checksum='013a489ebb3e24ef3d915abe5b94c3286c070dfe0818d5bca8108f1d6e8440d2' ;;
 	esac
 
 	remote_file "/tmp/go-${version}.tgz" \
