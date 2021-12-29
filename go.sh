f14b0b7aaf2ec5fb5da10414a5c78534d57c2d29
Chris Bandy <bandy.chris@gmail.com>
2021-12-28 18:44:53 -0600

    Update to Go 1.17.5

--- go.sh
+++ go.sh
@@ -4,18 +4,18 @@ set -eu
 
 export PATH="$HOME/.local/go/bin:$PATH"
 
-version='1.17.3'
+version='1.17.5'
 
 if [ "go${version}" != "$( read -ra array <<< "$(maybe go version)"; echo "${array[2]-}" )" ]
 then
 	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
 	case "$build" in
-		'darwin-amd64') checksum='765c021e372a87ce0bc58d3670ab143008dae9305a79e9fa83440425529bb636' ;;
-		'linux-amd64')  checksum='550f9845451c0c94be679faf116291e7807a8d78b43149f9506c1b15eb89008c' ;;
+		'darwin-amd64') checksum='2db6a5d25815b56072465a2cacc8ed426c18f1d5fc26c1fc8c4f5a7188658264' ;;
+		'linux-amd64')  checksum='bd78114b0d441b029c8fe0341f4910370925a4d270a6a590668840675b0c653e' ;;
 	esac
 
 	remote_file "/tmp/go-${version}.tgz" \
-		"https://golang.org/dl/go${version}.${build}.tar.gz" \
+		"https://go.dev/dl/go${version}.${build}.tar.gz" \
 		"$checksum"
 
 	tar --file "/tmp/go-${version}.tgz" --extract --directory '/tmp'
@@ -23,3 +23,5 @@ then
 fi
 
 mkdir -p "$HOME/go"
+
+go install golang.org/x/tools/gopls@latest
