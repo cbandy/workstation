60b892c4317d8aa8b9475ac3e775dd195411c7f6
Chris Bandy <bandy.chris@gmail.com>
2022-10-15 08:33:31 -0500

    Update to Go 1.19.2

--- go.sh
+++ go.sh
@@ -4,14 +4,14 @@ set -eu
 
 export PATH="$HOME/.local/go/bin:$PATH"
 
-version='1.17.5'
+version='1.19.2'
 
 if [ "go${version}" != "$( read -ra array <<< "$(maybe go version)"; echo "${array[2]-}" )" ]
 then
 	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
 	case "$build" in
-		'darwin-amd64') checksum='2db6a5d25815b56072465a2cacc8ed426c18f1d5fc26c1fc8c4f5a7188658264' ;;
-		'linux-amd64')  checksum='bd78114b0d441b029c8fe0341f4910370925a4d270a6a590668840675b0c653e' ;;
+		'darwin-amd64') checksum='16f8047d7b627699b3773680098fbaf7cc962b7db02b3e02726f78c4db26dfde' ;;
+		'linux-amd64')  checksum='5e8c5a74fe6470dd7e055a461acda8bb4050ead8c2df70f227e3ff7d8eb7eeb6' ;;
 	esac
 
 	remote_file "/tmp/go-${version}.tgz" \
