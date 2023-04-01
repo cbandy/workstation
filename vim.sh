44809390ce29df82011c544d1ae92ee64c7afc58
Chris Bandy <bandy.chris@gmail.com>
2023-04-01 12:33:06 -0500

    Update to Neovim 0.8.3

--- vim.sh
+++ vim.sh
@@ -4,14 +4,14 @@ set -eu
 
 export PATH="$HOME/.local/bin:$PATH"
 
-version='0.8.1'
+version='0.8.3'
 
 if [ "v${version}" != "$( read -ra array <<< "$(maybe nvim --version)"; echo "${array[1]-}" )" ]
 then
 	if [ "${OS[distribution]}" = 'macOS' ]; then
 		install_packages 'neovim'
 	else
-		checksum='25658f3db59fac398bc28b7ff7984ed1becdf9a3e98b3e4e226c236c35107054'
+		checksum='05f7a2cb177f4506882b7cd867d82207c79066e7a9550e4ef78e6eac537dd4a8'
 
 		silent command -v 'fusermount' || install_packages 'fuse'
 
