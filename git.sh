2b7144b2d77d00405848c1f619bf5fe87e69fd9d
Chris Bandy <bandy.chris@gmail.com>
2024-04-20 17:15:58 -0500

    Install Homebrew first and use the default prefix for arm64
    Start using ~/.local/homebrew as a link to the prefix.

--- git.sh
+++ git.sh
@@ -11,9 +11,9 @@ local_file "$HOME/.gitmsg.txt" "files/git/gitmsg.txt"
 silent command -v git || install_packages 'git'
 
 if ! silent command -v diff-highlight; then
-	install_file "$HOME/.local/bin/diff-highlight" "$(
+	install_file "${HOME}/.local/bin/diff-highlight" "$(
 		exists() { [ ! -f "$1" ] || echo "$1"; }
 		exists '/usr/share/doc/git/contrib/diff-highlight/diff-highlight'
-		exists "$HOME/.local/share/git-core/contrib/diff-highlight/diff-highlight"
+		exists "${HOME}/.local/homebrew/share/git-core/contrib/diff-highlight/diff-highlight"
 	)"
 fi
