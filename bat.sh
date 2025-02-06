de0d7708308110e351cfcbf5d8dc53061c0ff24c
Chris Bandy <bandy.chris@gmail.com>
2025-02-06 15:10:34 -0600

    Commit some theme files
    TextMate themes are increasingly hard to come by.
    
    See: https://www.github.com/tinted-theming
    See: https://www.github.com/sharkdp/bat/issues/2943

--- bat.sh
+++ bat.sh
@@ -10,10 +10,9 @@ silent command -v bat || install_packages 'bat'
 silent command -pv bat batcat &&
 	ln -fs "$(command -pv bat batcat)" "${HOME}/.local/bin/bat"
 
-local_file  "${HOME}/.config/bat/config" 'files/bat/config'
-mkdir -p    "${HOME}/.config/bat/themes"
-remote_file "${HOME}/.config/bat/themes/base16-tomorrow-night.tmTheme" \
-	'https://raw.githubusercontent.com/chriskempson/base16-textmate/master/Themes/base16-tomorrow-night.tmTheme' \
-	'5680df17a0dde9be1dbbbc2a26d5f41359d655e01232027139bf0ebb149553e4'
+mkdir -p   "${HOME}/.config/bat/themes"
+local_file "${HOME}/.config/bat/config" 'files/bat/config'
+local_file "${HOME}/.config/bat/themes/base16-tomorrow-night.tmTheme" \
+	'files/themes/base16-tomorrow-night.tmTheme'
 
 bat cache --build
