d66851e44e3921503aa6d8b28eccc80fdddc751b
Chris Bandy <bandy.chris@gmail.com>
2025-02-13 22:54:04 -0600

    Update to Go 1.24.0

--- go.sh
+++ go.sh
@@ -4,15 +4,15 @@ set -eu
 
 export PATH="$HOME/.local/go/bin:$PATH"
 
-version='1.23.2'
+version='1.24.0'
 
 if [ "go${version}" != "$( read -ra array <<< "$(maybe go version)"; echo "${array[2]-}" )" ]
 then
 	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
 	case "$build" in
-		'darwin-amd64') checksum='445c0ef19d8692283f4c3a92052cc0568f5a048f4e546105f58e991d4aea54f5' ;;
-		'darwin-arm64') checksum='d87031194fe3e01abdcaf3c7302148ade97a7add6eac3fec26765bcb3207b80f' ;;
-		'linux-amd64')  checksum='542d3c1705f1c6a1c5a80d5dc62e2e45171af291e755d591c5e6531ef63b454e' ;;
+		'darwin-amd64') checksum='7af054e5088b68c24b3d6e135e5ca8d91bbd5a05cb7f7f0187367b3e6e9e05ee' ;;
+		'darwin-arm64') checksum='fd9cfb5dd6c75a347cfc641a253f0db1cebaca16b0dd37965351c6184ba595e4' ;;
+		'linux-amd64')  checksum='dea9ca38a0b852a74e81c26134671af7c0fbe65d81b0dc1c5bfe22cf7d4c8858' ;;
 	esac
 
 	remote_file "/tmp/go-${version}.tgz" \
