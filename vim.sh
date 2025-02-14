3ae1e403f3aba2ae6a0aab2f4dda26e63d407783
Chris Bandy <bandy.chris@gmail.com>
2025-02-13 22:04:05 -0600

    Update to Neovim 0.10.4

--- vim.sh
+++ vim.sh
@@ -4,20 +4,23 @@ set -eu
 
 export PATH="$HOME/.local/bin:$PATH"
 
-version='0.10.2'
+version='0.10.4'
 
 if [ "v${version}" != "$( read -ra array <<< "$(maybe nvim --version)"; echo "${array[1]-}" )" ]
 then
 	if [ "${OS[distribution]}" = 'macOS' ]; then
 		install_packages 'neovim'
 	else
-		checksum='970113b5279718126b82adddf3fbed1741db7afabf8a4134635a0d75c9f2c5d8'
-		project='https://github.com/neovim/neovim'
+		build="${OS[kernel],,}-${OS[machine]}"
+		case "$build" in
+			'linux-arm64')  checksum='cc02d640809f6cfb27ee5fca78cc612ccda0dc4fbb648430d49de623660d0216' ;;
+			'linux-x86_64') checksum='d1b1488ef6a970dc3a2826b788d31d81b2f308ad42ef70ed62d35d0bf6189336' ;;
+		esac
 
 		silent command -v 'fusermount' || install_packages 'fuse'
 
 		remote_file "/tmp/neovim-${version}" \
-			"${project}/releases/download/v${version}/nvim.appimage" \
+			"https://github.com/neovim/neovim/releases/download/v${version}/nvim-${build}.appimage" \
 			"${checksum}"
 
 		install_file "$HOME/.local/bin/nvim" "/tmp/neovim-${version}"
