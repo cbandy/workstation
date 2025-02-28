992823328ba1f905d1200d7834cc746f54781112
Chris Bandy <bandy.chris@gmail.com>
2025-02-27 20:04:06 -0600

    Update to ruby-install 0.10.1
    This version automatically builds with multiple CPUs.
    
    See: https://github.com/postmodern/ruby-install/commit/e7b918f31397cd08

--- ruby.sh
+++ ruby.sh
@@ -7,22 +7,26 @@ export PATH="$HOME/.local/bin:$PATH"
 local_file "$HOME/.gemrc" "files/ruby/gemrc"
 local_file "$HOME/.rspec" "files/ruby/rspec"
 
-if ! silent command -v chruby-exec; then
+version='0.3.9'
+
+if [ "${version}" != "$( read -ra array <<< "$(maybe chruby-exec --version)"; echo "${array[2]-}" )" ]
+then
 	checksum='7220a96e355b8a613929881c091ca85ec809153988d7d691299e0a16806b42fd'
-	version='0.3.9'
 
 	remote_file "/tmp/chruby-${version}.tgz" "https://github.com/postmodern/chruby/archive/v${version}.tar.gz" "$checksum"
 	tar  --file "/tmp/chruby-${version}.tgz" --extract --directory '/tmp'
 	make --directory "/tmp/chruby-${version}" install PREFIX="$HOME/.local"
 fi
 
-if ! silent command -v ruby-install; then
-	checksum='500a8ac84b8f65455958a02bcefd1ed4bfcaeaa2bb97b8f31e61ded5cd0fd70b'
-	version='0.7.0'
+version='0.10.1'
+
+if [ "${version}" != "$( read -ra array <<< "$(maybe ruby-install --version)"; echo "${array[2]-}" )" ]
+then
+	checksum='af09889b55865fc2a04e337fb4fe5632e365c0dce871556c22dfee7059c47a33'
 
 	remote_file "/tmp/ruby-install-${version}.tgz" "https://github.com/postmodern/ruby-install/archive/v${version}.tar.gz" "$checksum"
 	tar  --file "/tmp/ruby-install-${version}.tgz" --extract --directory '/tmp'
 	make --directory "/tmp/ruby-install-${version}" install PREFIX="$HOME/.local"
 fi
 
-ruby-install --jobs="${OS[processors]}" --no-reinstall ruby -- --disable-install-doc
+ruby-install --cleanup --no-reinstall ruby -- --disable-install-doc
