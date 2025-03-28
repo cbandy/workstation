5c00a4d0a3e1b9519f356e52bd84e98eeaf36480
Chris Bandy <bandy.chris@gmail.com>
2025-03-27 22:32:47 -0500

    Install pyenv

--- python.sh
+++ python.sh
@@ -14,5 +14,15 @@ if ! silent command -v pip3; then
 	fi
 fi
 
+# https://github.com/pyenv/pyenv-installer
+while read -r src dst; do
+  [ -d "${dst}" ] || git clone --config 'advice.detachedHead=0' --depth=1 "${src}" "${dst}"
+done << PyEnv
+https://github.com/pyenv/pyenv.git        ${HOME}/.pyenv
+https://github.com/pyenv/pyenv-update.git ${HOME}/.pyenv/plugins/pyenv-update
+PyEnv
+
+# TODO: packages: libbz2-dev libsqlite3-dev liblzma-dev
+
 silent command -v pygmentize || pip3 install --user pygments
 pygmentize -L styles | grep --silent base16 || pip3 install --user pygments-base16
