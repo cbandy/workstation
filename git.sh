a0c7bdaa59378fbcab1ec186379de7ad73cf6cb2
Chris Bandy <bandy.chris@gmail.com>
2025-03-30 18:01:36 -0500

    Use delta to display Git diffs

--- git.sh
+++ git.sh
@@ -2,21 +2,13 @@
 set -eu
 . share/functions.sh
 
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="${HOME}/.local/bin:${PATH}"
 
-local_file "$HOME/.gitconfig"  "files/git/gitconfig"
-local_file "$HOME/.gitignore"  "files/git/gitignore"
-local_file "$HOME/.gitmsg.txt" "files/git/gitmsg.txt"
+local_file "${HOME}/.gitconfig"  'files/git/gitconfig'
+local_file "${HOME}/.gitignore"  'files/git/gitignore'
+local_file "${HOME}/.gitmsg.txt" 'files/git/gitmsg.txt'
 
 silent command -v git || install_packages 'git'
-
-if ! silent command -v diff-highlight; then
-	install_file "$HOME/.local/bin/diff-highlight" "$(
-		exists() { [ ! -f "$1" ] || echo "$1"; }
-		exists '/usr/share/doc/git/contrib/diff-highlight/diff-highlight'
-		exists "$HOME/.local/share/git-core/contrib/diff-highlight/diff-highlight"
-	)"
-fi
 
 version='0.18.2'
 
