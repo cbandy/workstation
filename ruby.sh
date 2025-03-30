9d9abed865b6f1b8e53559bbc668893103b3d219
Chris Bandy <bandy.chris@gmail.com>
2025-03-30 18:03:13 -0500

    Make it easier to follow links to chruby and ruby-install

--- ruby.sh
+++ ruby.sh
@@ -2,20 +2,21 @@
 set -eu
 . share/functions.sh
 
-export PATH="$HOME/.local/bin:$PATH"
+export PATH="${HOME}/.local/bin:${PATH}"
 
-local_file "$HOME/.gemrc" "files/ruby/gemrc"
-local_file "$HOME/.rspec" "files/ruby/rspec"
+local_file "${HOME}/.gemrc" "files/ruby/gemrc"
+local_file "${HOME}/.rspec" "files/ruby/rspec"
 
 version='0.3.9'
 
 if [ "${version}" != "$( read -ra array <<< "$(maybe chruby-exec --version)"; echo "${array[2]-}" )" ]
 then
 	checksum='7220a96e355b8a613929881c091ca85ec809153988d7d691299e0a16806b42fd'
+	project='https://github.com/postmodern/chruby'
 
-	remote_file "/tmp/chruby-${version}.tgz" "https://github.com/postmodern/chruby/archive/v${version}.tar.gz" "$checksum"
+	remote_file "/tmp/chruby-${version}.tgz" "${project}/archive/v${version}.tar.gz" "${checksum}"
 	tar  --file "/tmp/chruby-${version}.tgz" --extract --directory '/tmp'
-	make --directory "/tmp/chruby-${version}" install PREFIX="$HOME/.local"
+	make --directory "/tmp/chruby-${version}" install PREFIX="${HOME}/.local"
 fi
 
 version='0.10.1'
@@ -23,10 +24,11 @@ version='0.10.1'
 if [ "${version}" != "$( read -ra array <<< "$(maybe ruby-install --version)"; echo "${array[2]-}" )" ]
 then
 	checksum='af09889b55865fc2a04e337fb4fe5632e365c0dce871556c22dfee7059c47a33'
+	project='https://github.com/postmodern/ruby-install'
 
-	remote_file "/tmp/ruby-install-${version}.tgz" "https://github.com/postmodern/ruby-install/archive/v${version}.tar.gz" "$checksum"
+	remote_file "/tmp/ruby-install-${version}.tgz" "${project}/archive/v${version}.tar.gz" "${checksum}"
 	tar  --file "/tmp/ruby-install-${version}.tgz" --extract --directory '/tmp'
-	make --directory "/tmp/ruby-install-${version}" install PREFIX="$HOME/.local"
+	make --directory "/tmp/ruby-install-${version}" install PREFIX="${HOME}/.local"
 fi
 
 ruby-install --cleanup --no-reinstall ruby -- --disable-install-doc
