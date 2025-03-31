a033685042402efb4fafaa58ac1635f98369ba0e
Chris Bandy <bandy.chris@gmail.com>
2025-03-30 22:00:56 -0500

    Install LuaLS and Tree-sitter before Neovim plugins

--- vim.sh
+++ vim.sh
@@ -1,42 +1,96 @@
 #!/usr/bin/env bash
-set -eu
 . share/functions.sh
 
-export PATH="$HOME/.local/bin:$PATH"
+shopt -s -o errexit nounset
+export PATH="${HOME}/.local/bin:${HOME}/.local/luals/bin:${PATH}"
 
+read -r _ current _ <<< "$(maybe nvim --version ||:)"
 version='0.11.0'
 
-if [ "v${version}" != "$( read -ra array <<< "$(maybe nvim --version)"; echo "${array[1]-}" )" ]
-then
-	if [ "${OS[distribution]}" = 'macOS' ]; then
-		install_packages 'neovim'
-	else
-		build="${OS[kernel],,}-${OS[machine]}"
-		case "$build" in
-			'linux-arm64')  checksum='307972fd1e14f68e3a24c23a48e91387399385a3cf3d80e319542c01efe3bcf2' ;;
-			'linux-x86_64') checksum='ca44cd43fe8d55418414496e8ec7bac83f611705ece167f4ccb93cbf46fec6c0' ;;
-		esac
+if [[ "${current}" == "v${version}" ]]; then
+	:
+elif [[ "${OS[distribution]}" == 'macOS' ]]; then
+	install_packages 'neovim'
+else
+	silent command -v 'fusermount' || install_packages 'fuse'
 
-		silent command -v 'fusermount' || install_packages 'fuse'
+	project='https://github.com/neovim/neovim'
+	build="${OS[kernel],,}-${OS[machine]}"
+	case "${build}" in
+		'linux-arm64')  checksum='307972fd1e14f68e3a24c23a48e91387399385a3cf3d80e319542c01efe3bcf2' ;;
+		'linux-x86_64') checksum='ca44cd43fe8d55418414496e8ec7bac83f611705ece167f4ccb93cbf46fec6c0' ;;
+		*) error "missing checksum for ${build}" ;;
+	esac
 
-		remote_file "/tmp/neovim-${version}" \
-			"https://github.com/neovim/neovim/releases/download/v${version}/nvim-${build}.appimage" \
-			"${checksum}"
+	remote_file "/tmp/neovim-${version}" \
+		"${project}/releases/download/v${version}/nvim-${build}.appimage" "${checksum}"
 
-		install_file "$HOME/.local/bin/nvim" "/tmp/neovim-${version}"
-	fi
+	install_file "${HOME}/.local/bin/nvim" "/tmp/neovim-${version}"
 fi
 
-if [ ! -d "$HOME/.config/nvim" ]; then
-	mkdir -p "$HOME/.config"
+current=$(maybe lua-language-server --version ||:)
+version='3.13.9'
 
-	git clone 'https://github.com/cbandy/vim-config.git' "$HOME/.config/nvim"
+if [[ "${current}" == "${version}" ]]; then
+	:
+elif [[ "${OS[distribution]}" == 'macOS' ]]; then
+	install_packages 'lua-language-server'
+else
+	project='https://github.com/LuaLS/lua-language-server'
+	build="$(ldd --version 2>&1)"
+	[[ "${build}" == *musl* ]] && build='-musl'
+	[[ "${build}" != *musl* ]] && build=''
 
-	( cd "$HOME/.config/nvim" && nvim -u plugins.vim '+PlugUpgrade' '+PlugInstall' '+qa' )
+	build="${OS[kernel],,}-${OS[machine]/86_/}${build}"
+	case "${build}" in
+		'linux-arm64') checksum='0857343d82cdbc01ce2ed56b548358ec90c351f2d0db28c567d1198d531018df' ;;
+		'linux-x64')   checksum='17642b2154446c0a15c7f6d335242f071d6910ce1e76c7cd95a29b64dfae1348' ;;
+		*) error "missing checksum for ${build}" ;;
+	esac
+
+	remote_file "/tmp/luals-${version}.tar" \
+		"${project}/releases/download/${version}/lua-language-server-${version}-${build}.tar.gz" \
+		"${checksum}"
+
+	[[ ! -d '/tmp/luals' ]] || rm -r '/tmp/luals' && mkdir '/tmp/luals'
+	tar --file "/tmp/luals-${version}.tar" --extract --directory '/tmp/luals'
+
+	[[ ! -d "${HOME}/.local/luals" ]] || rm -r "${HOME}/.local/luals" &&
+		mv '/tmp/luals' "${HOME}/.local/luals"
 fi
 
-if [ ! -d "$HOME/.vim" ]; then
-	ln -s "$HOME/.config/nvim" "$HOME/.vim"
-	ln -s "$HOME/.vim/vimrc"   "$HOME/.vimrc"
-	ln -s "$HOME/.vim/gvimrc"  "$HOME/.gvimrc"
+read -r _ current _ <<< "$(maybe tree-sitter --version ||:)"
+version='0.25.3'
+
+if [[ "${current}" == "${version}" ]]; then
+	:
+elif [[ "${OS[distribution]}" == 'macOS' ]]; then
+	install_packages 'tree-sitter'
+else
+	project='https://github.com/tree-sitter/tree-sitter'
+	build="${OS[kernel],,}-${OS[machine]/86_/}"
+	case "${build}" in
+		'linux-arm64') checksum='ead1d0bdb0d8daeb1b27833635790bc673403e53fb80b4f61c9b3db7bd446151' ;;
+		'linux-x64')   checksum='ad69bfbd54f9ebba1387bf0e7c2683896a5968f2f78768380af70f0c4dd8ceab' ;;
+		*) error "missing checksum for ${build}" ;;
+	esac
+
+	remote_file "/tmp/treesitter-${version}.gz" \
+		"${project}/releases/download/v${version}/tree-sitter-${build}.gz" "${checksum}"
+	gunzip --force "/tmp/treesitter-${version}.gz"
+	install_file "${HOME}/.local/bin/tree-sitter" "/tmp/treesitter-${version}"
+fi
+
+if [[ ! -d "${HOME}/.config/nvim" ]]; then
+	mkdir -p "${HOME}/.config"
+
+	git clone 'https://github.com/cbandy/vim-config.git' "${HOME}/.config/nvim"
+
+	( cd "${HOME}/.config/nvim" && ./lua/plugins.lua '+PlugUpgrade' '+PlugInstall' '+qa' )
+fi
+
+if [[ ! -d "${HOME}/.vim" ]]; then
+	ln -s "${HOME}/.config/nvim" "${HOME}/.vim"
+	ln -s "${HOME}/.vim/vimrc"   "${HOME}/.vimrc"
+	ln -s "${HOME}/.vim/gvimrc"  "${HOME}/.gvimrc"
 fi
