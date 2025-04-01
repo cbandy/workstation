f9c231d2f6aca2749161e3a7889afbe7c0d83dc0
Chris Bandy <bandy.chris@gmail.com>
2025-03-31 21:35:49 -0500

    Install gotestsum when installing Go
    See: https://pkg.go.dev/gotest.tools/gotestsum

--- go.sh
+++ go.sh
@@ -1,32 +1,36 @@
 #!/usr/bin/env bash
-set -eu
 . share/functions.sh
 
-export PATH="$HOME/.local/go/bin:$PATH"
+shopt -s -o errexit nounset
+export PATH="${HOME}/.local/go/bin:${PATH}"
 
+read -r _ _ current _ <<< "$(maybe go version ||:)"
 version='1.24.1'
 
-if [ "go${version}" != "$( read -ra array <<< "$(maybe go version)"; echo "${array[2]-}" )" ]
-then
+if [[ "${current}" == "go${version}" ]]; then
+	:
+else
 	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
-	case "$build" in
+	case "${build}" in
 		'darwin-amd64') checksum='addbfce2056744962e2d7436313ab93486660cf7a2e066d171b9d6f2da7c7abe' ;;
 		'darwin-arm64') checksum='295581b5619acc92f5106e5bcb05c51869337eb19742fdfa6c8346c18e78ff88' ;;
 		'linux-amd64')  checksum='cb2396bae64183cdccf81a9a6df0aea3bce9511fc21469fb89a0c00470088073' ;;
+		*) error "missing checksum for ${build}" ;;
 	esac
 
 	remote_file "/tmp/go-${version}.tgz" \
 		"https://go.dev/dl/go${version}.${build}.tar.gz" \
-		"$checksum"
+		"${checksum}"
 
 	tar --file "/tmp/go-${version}.tgz" --extract --directory '/tmp'
-	[ ! -d "$HOME/.local/go" ] || rm -r "$HOME/.local/go" && mv '/tmp/go' "$HOME/.local/go"
+	[[ ! -d "${HOME}/.local/go" ]] || rm -r "${HOME}/.local/go" && mv '/tmp/go' "${HOME}/.local/go"
 fi
 
-mkdir -p "$HOME/go"
+mkdir -p "${HOME}/go"
 
+go install github.com/direnv/direnv/v2@latest
 go install golang.org/x/perf/cmd/benchstat@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install golang.org/x/tools/gopls@latest
 go install golang.org/x/vuln/cmd/govulncheck@latest
-go install github.com/direnv/direnv/v2@latest
+go install gotest.tools/gotestsum@latest
