ad2b1b159f0ae72806cb56eddb9f4dd16d44d6ca
Chris Bandy <bandy.chris@gmail.com>
2025-03-31 21:11:06 -0500

    Fix Obsidian desktop file and add an icon

--- obsidian.sh
+++ obsidian.sh
@@ -1,13 +1,30 @@
 #!/usr/bin/env bash
-set -eu
 . share/functions.sh
+
+shopt -s -o errexit nounset
 
 if [[ "${OS[distribution]}" == 'macOS' ]]; then
 	install_packages 'obsidian'
 else
 	open 'https://obsidian.md/download'
-	local_file "${HOME}/.local/share/applications/obsidian.desktop" 'files/obsidian/obsidian.desktop'
-	install_packages 'libnss3-dev'
+	install_packages 'libfuse2' 'libnss3-dev' 'librsvg2-bin'
 
+	# Convert the perfectly fine SVG to PNG so Garcon can pass it to ChromeOS.
+	# Use a transparent background to avoid the square-in-circle nonsense.
+	#
+	# https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/vm_tools/garcon/#fetching-icons
+	mkdir -p "${HOME}/.local/share/icons/hicolor/256x256/apps"
+	rsvg-convert --height=256 --width=256 --keep-aspect-ratio --background-color='rgba(0,0,0,0)' \
+		'files/obsidian/obsidian-logo-gradient.svg' > \
+		"${HOME}/.local/share/icons/hicolor/256x256/apps/obsidian.png"
+
+	# Notify Garcon that Obsidian is installed by creating a desktop file.
+	#
+	# https://specifications.freedesktop.org/desktop-entry-spec
+	# https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/vm_tools/garcon/#installed-applications
+	mkdir -p "${HOME}/.local/share/applications"
+	local_file "${HOME}/.local/share/applications/obsidian.desktop" 'files/obsidian/obsidian.desktop'
+
+	echo
 	echo Download the AppImage and name it ~/.local/bin/obsidian
 fi
