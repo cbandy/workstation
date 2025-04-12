7b0a3f6ab17e08e99e9c85ec0a18e3338c1bffed
Chris Bandy <bandy.chris@gmail.com>
2025-04-12 00:09:02 -0500

    Install fd along with fzf

--- shell.sh
+++ shell.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091
-set -eu
 . share/functions.sh
 : "${OS[distribution]:?}"
 
+shopt -s -o errexit nounset
 export PATH="${HOME}/.local/bin:${PATH}"
 
 mkdir -p "${HOME}/.config" "${HOME}/.local/bin"
@@ -33,7 +33,7 @@ silent command -v zip    || packages+=('zip')
 version='0.10.0'
 
 case "$(maybe shellcheck --version)" in *"version: ${version}"*) :;; *)
-	if [[ "${OS[distribution]}" = 'macOS' ]]; then
+	if [[ "${OS[distribution]}" == 'macOS' ]]; then
 		install_packages 'shellcheck'
 	else
 		build="${OS[kernel],,}.${OS[machine]}"
@@ -51,10 +51,38 @@ case "$(maybe shellcheck --version)" in *"version: ${version}"*) :;; *)
 	fi
 esac
 
+current=$(maybe fd --version ||:)
+version='10.2.0'
+
+if [[ "${current}" == "fd ${version}" ]]; then
+	:
+elif [[ "${OS[distribution]}" == 'macOS' ]]; then
+	install_packages 'fzf'
+else
+	build=$(ldd --version 2>&1 ||:)
+	[[ "${build}" == *musl* ]] && build='unknown-linux-musl'
+	[[ "${build}" != *musl* ]] && build='unknown-linux-gnu'
+	build="${OS[machine]}-${build}"
+	project='https://github.com/sharkdp/fd'
+
+	case "${build}" in
+		'x86_64-unknown-linux-gnu') checksum='5f9030bcb0e1d03818521ed2e3d74fdb046480a45a4418ccff4f070241b4ed25' ;;
+		*) error "unexpected: ${build}" ;;
+	esac
+
+	remote_file "/tmp/fd-${version}.tar" \
+		"${project}/releases/download/v${version}/fd-v${version}-${build}.tar.gz" \
+		"${checksum}"
+
+	tar --file "/tmp/fd-${version}.tar" --extract --directory '/tmp'
+	install_file "${HOME}/.local/bin/fd" "/tmp/fd-v${version}-${build}/fd"
+fi
+
+current=$(maybe fzf --version ||:)
 version='0.60.2'
 
-case "$(maybe fzf --version)" in "${version} "*) :;; *)
-	if [[ "${OS[distribution]}" = 'macOS' ]]; then
+case "${current}" in "${version} "*) :;; *)
+	if [[ "${OS[distribution]}" == 'macOS' ]]; then
 		install_packages 'fzf'
 	else
 		build="${OS[kernel],,}_${OS[machine]/x86_/amd}"
@@ -62,9 +90,10 @@ case "$(maybe fzf --version)" in "${version} "*) :;; *)
 			'linux_amd64') checksum='f459d9c0676edfcd4a717efc48ea7768d395d5745872d34ae338452017381839' ;;
 			*) error "unexpected: ${build}" ;;
 		esac
+		project='https://github.com/junegunn/fzf'
 
 		remote_file "/tmp/fzf-${version}.tar" \
-			"https://github.com/junegunn/fzf/releases/download/v${version}/fzf-${version}-${build}.tar.gz" \
+			"${project}/releases/download/v${version}/fzf-${version}-${build}.tar.gz" \
 			"${checksum}"
 
 		tar --file "/tmp/fzf-${version}.tar" --extract --directory '/tmp'
