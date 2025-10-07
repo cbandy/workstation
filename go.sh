d962a67f8aad38c29e940306a34f854659414b30
Chris Bandy <bandy.chris@gmail.com>
2025-10-07 16:26:09 -0500

    Update to Go 1.24.8

--- go.sh
+++ go.sh
@@ -2,19 +2,19 @@
 . share/functions.sh
 
 shopt -s -o errexit nounset
-export PATH="${HOME}/.local/go/bin:${PATH}"
+PATH="${HOME}/.local/go/bin:${PATH}"
 
 read -r _ _ current _ <<< "$(maybe go version ||:)"
-version='1.24.4'
+version='1.24.8'
 
 if [[ "${current}" == "go${version}" ]]; then
 	:
 else
 	build="${OS[kernel],,}-${OS[machine]/x86_/amd}"
 	case "${build}" in
-		'darwin-amd64') checksum='69bef555e114b4a2252452b6e7049afc31fbdf2d39790b669165e89525cd3f5c' ;;
-		'darwin-arm64') checksum='27973684b515eaf461065054e6b572d9390c05e69ba4a423076c160165336470' ;;
-		'linux-amd64')  checksum='77e5da33bb72aeaef1ba4418b6fe511bc4d041873cbf82e5aa6318740df98717' ;;
+		'darwin-amd64') checksum='ecb3cecb1e0bcfb24e50039701f9505b09744cc4730a8b9fc512b0a3b47cf232' ;;
+		'darwin-arm64') checksum='0db27ff8c3e35fd93ccf9d31dd88a0f9c6454e8d9b30c28bd88a70b930cc4240' ;;
+		'linux-amd64')  checksum='6842c516ca66c89d648a7f1dbe28e28c47b61b59f8f06633eb2ceb1188e9251d' ;;
 		*) error "missing checksum for ${build}" ;;
 	esac
 
@@ -25,8 +25,6 @@ else
 	tar --file "/tmp/go-${version}.tgz" --extract --directory '/tmp'
 	[[ ! -d "${HOME}/.local/go" ]] || rm -r "${HOME}/.local/go" && mv '/tmp/go' "${HOME}/.local/go"
 fi
-
-mkdir -p "${HOME}/go"
 
 go install github.com/direnv/direnv/v2@latest
 go install golang.org/x/perf/cmd/benchstat@latest
