779106b48f92eb3c7f93ac38f196020dfa01659c
Chris Bandy <bandy.chris@gmail.com>
2026-02-17 14:32:11 -0600

    Explain the local symlink to bat

--- bat.sh
+++ bat.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
-set -eu
 . share/functions.sh
 
-export PATH="${HOME}/.local/bin:${PATH}"
+shopt -s -o errexit nounset
+PATH="${HOME}/.local/bin:${PATH}"
+
 mkdir -p "${HOME}/.local/bin"
 
 silent command -v bat || install_packages 'bat'
 
+# Debian packages deliver the executable as 'batcat'. Regardless, create
+# a local link named 'bat' to whichever `command -p` finds on the system PATH.
+#
+# https://github.com/sharkdp/bat#installation
 silent command -pv bat batcat &&
-	ln -fs "$(command -pv bat batcat)" "${HOME}/.local/bin/bat"
+	ln -fs "$(command -pv bat batcat ||:)" "${HOME}/.local/bin/bat"
 
 mkdir -p   "${HOME}/.config/bat/themes"
 local_file "${HOME}/.config/bat/config" 'files/bat/config'
