e54fc84352a0d01d6fae4f90b1934375d97129c7
Chris Bandy <bandy.chris@gmail.com>
2026-02-19 13:27:10 -0600

    Make Applications and the App Store optional

--- mac.sh
+++ mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-set -eu
-test "$(uname)" = 'Darwin' || exit 0
+test "$(uname ||:)" = 'Darwin' || exit 0
 
+shopt -s -o errexit nounset
 PATH="${HOME}/.local/homebrew/bin:${PATH}"
 PATH="${HOME}/.local/bin:${PATH}"
 
@@ -11,6 +11,8 @@ PATH="${HOME}/.local/bin:${PATH}"
 # Appearance > Windows > Heavy border around windows…
 # Profiles > Colors > Color Presets… > Import… > [files/themes/base16-tomorrow-night.itermcolors]
 # Profiles > Text > Font
+
+xcode-select --print-path || xcode-select --install
 
 # Homebrew configuration centers around environment variables.
 # It automatically loads from a brew.env file, but it looks in $XDG_CONFIG_HOME only when $XDG_CONFIG_HOME is set.
@@ -36,44 +38,19 @@ else
 	brew update --force
 fi
 
-packages=(
-	'bash-completion@2'
-	'gh'
-	'git'
-	'gnupg'
-	'mas'
-	'ripgrep'
-)
+brew install --quiet 'bash-completion@2' # includes updated Bash
+brew install --quiet 'container'  # https://github.com/apple/container
+brew install --quiet 'iterm2'     # https://iterm2.com       → https://github.com/gnachman/iTerm2
+brew install --quiet 'meetingbar' # https://meetingbar.app   → https://github.com/leits/MeetingBar
+brew install --quiet 'rectangle'  # https://rectangleapp.com → https://github.com/rxhanson/Rectangle
+brew install --quiet 'todoist'    # https://todoist.com      → https://github.com/doist
+brew install --quiet 'textual'    # https://codeux.com/textual
 
-for package in "${packages[@]}"; do
-	brew list "$package" &> /dev/null || brew install "$package"
-done
+# Indicate to [files/shell/bashrc] exactly which Bash to use.
+[[ -x "${HOME}/.local/bin/bash" ]] || ( cd "${HOME}/.local/bin" && ln -sf '../homebrew/bin/bash' . )
 
-apps=(
-	'552792489:Status Clock'
-	'1538878817:UTM'
-	'1451685025:WireGuard'
-)
-
-for app in "${apps[@]}"; do
-	if ! grep -q "^${app%%:*} " <<< "$(mas list)" &&
-		grep -q "^${app#*:}" <<< "$(mas info "${app%%:*}")"
-	then
-		mas install "${app%%:*}"
-	fi
-done
-
-applications=(
-	'Calculator'
-	'Preview'
-	'StatusClock'
-	'System Settings'
-	'TextEdit'
-	'UTM'
-	'WireGuard'
-)
-
-for application in "${applications[@]}"; do
+application_shortcut() {
+	local -r application="$1"
 	osascript <<-APPLESCRIPT
 	tell application "Finder"
 		if not (exists (path to applications folder from user domain as text) & "${application}") then
@@ -83,31 +60,44 @@ for application in "${applications[@]}"; do
 		end if
 	end tell
 	APPLESCRIPT
+}
+
+while [[ "$#" -gt 0 ]]; do
+	case "${1-}" in
+		'--app-links')
+			echo "✨ shortcuts to /Applications"
+
+			application_shortcut 'Calculator'
+			application_shortcut 'Preview'
+			application_shortcut 'System Settings'
+			application_shortcut 'TextEdit'
+			;;
+		'--app-store')
+			brew install --quiet 'mas'
+			# TODO: move into function
+			for app in \
+				'552792489:Status Clock' \
+				'1538878817:UTM' \
+				'1451685025:WireGuard' \
+			; do
+				if ! grep -q "^${app%%:*} " <<< "$(mas list ||:)" &&
+					grep -q "^${app#*:}" <<< "$(mas info "${app%%:*}" ||:)"
+				then
+					mas install "${app%%:*}"
+				fi
+			done
+
+			application_shortcut 'StatusClock'
+			application_shortcut 'UTM'
+			application_shortcut 'WireGuard'
+
+			if [[ -x '/Applications/UTM.app/Contents/MacOS/utmctl' ]] &&
+				! command -v utmctl &> /dev/null
+			then
+				ln -sf '/Applications/UTM.app/Contents/MacOS/utmctl' "${HOME}/.local/bin/utmctl"
+			fi
+			;;
+		*)
+	esac
+	shift
 done
-
-applications=(
-	'grammarly-desktop'
-	'iterm2'
-	'keeweb'
-	'meetingbar'
-	'rancher'
-	'rectangle'
-	'slack'
-	'todoist'
-	'textual'
-	'visual-studio-code'
-)
-
-for application in "${applications[@]}"; do
-	brew list "$application" &> /dev/null || brew install "$application"
-done
-
-brew list 'librewolf' &> /dev/null || brew install --no-quarantine 'librewolf'
-
-[[ -x "${HOME}/.local/bin/bash" ]] || (cd "${HOME}/.local/bin" && ln -sf '../homebrew/bin/bash' .)
-
-if [ -x '/Applications/UTM.app/Contents/MacOS/utmctl' ] && ! command -v utmctl &> /dev/null; then
-	ln -sf '/Applications/UTM.app/Contents/MacOS/utmctl' "${HOME}/.local/bin/utmctl"
-fi
-
-xcode-select --print-path || xcode-select --install
