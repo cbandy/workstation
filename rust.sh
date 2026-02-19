228051394941f87c3c54eb85f16072fb8a6d8d02
Chris Bandy <bandy.chris@gmail.com>
2026-02-18 19:46:35 -0600

    Update to rustup 1.28.2

--- rust.sh
+++ rust.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
-set -eu
 . share/functions.sh
+: "${OS[distribution]:?}"
 
-version='1.27.1'
+shopt -s -o errexit nounset
+PATH="${HOME}/.cargo/bin:${HOME}/.local/bin:${PATH}"
 
-if true
-then
-	if "${OS[distribution]}" == 'macOS'; then
-		install_packages 'rustup'
-	else
-		build="$(ldd --version 2>&1)"
-		[[ "${build}" == *musl* ]] && build='unknown-linux-musl'
-		[[ "${build}" != *musl* ]] && build='unknown-linux-gnu'
-		build="${OS[machine]}-${build}"
+current=$(maybe rustup --version ||:)
+version='1.28.2'
 
-		case "$build" in
-			'x86_64-unknown-linux-gnu') checksum='6aeece6993e902708983b209d04c0d1dbb14ebb405ddb87def578d41f920f56d' ;;
-		esac
+case "${current}" in "rustup ${version}"*) :;; *)
+	case "${OS[distribution]}" in
+		'macOS') install_packages 'rustup' ;;
+		*)
+			build=$(ldd --version 2>&1)
+			[[ "${build}" == *musl* ]] && build='unknown-linux-musl'
+			[[ "${build}" != *musl* ]] && build='unknown-linux-gnu'
+			build="${OS[machine]}-${build}"
 
-		remote_file '/tmp/rustup-init' \
-			"https://static.rust-lang.org/rustup/dist/${build}/rustup-init" \
-			"${checksum}"
-	fi
-fi
+			case "${build}" in
+				'aarch64-unknown-linux-gnu') checksum='sha256:e3853c5a252fca15252d07cb23a1bdd9377a8c6f3efa01531109281ae47f841c' ;;
+				'x86_64-unknown-linux-gnu')  checksum='sha256:20a06e644b0d9bd2fbdbfd52d42540bdde820ea7df86e92e533c073da0cdd43c' ;;
+				*) error "missing checksum for ${build}" ;;
+			esac
+
+			remote_file "/tmp/rustup-init-${version}" \
+				"https://static.rust-lang.org/rustup/archive/${version}/${build}/rustup-init" \
+				"${checksum}"
+
+			install_file /tmp/rustup-init "/tmp/rustup-init-${version}"
+			/tmp/rustup-init --no-modify-path -y
+			rm /tmp/rustup-init
+			;;
+	esac
+esac
+
+# https://github.com/pgcentralfoundation/pgrx#system-requirements
+case "${OS[distribution]}" in
+	'debian'|'ubuntu') install_packages 'libclang-dev' ;;
+	'fedora'|'rhel') install_packages 'clang' ;;
+	*) ;;
+esac
+
+rustup component add rust-analyzer
+rustup update
