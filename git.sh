7c353cf62712fb2d2a5cd835d3ac20cfeb3b47cd
Chris Bandy <bandy.chris@gmail.com>
2026-02-18 21:51:00 -0600

    Add checksums for delta on arm64

--- git.sh
+++ git.sh
@@ -20,13 +20,20 @@ case "${current}" in *"${version}") ;; *)
 	case "${OS[distribution]}" in
 		'fedora'|'macOS'|'rhel') install_packages 'git-delta' ;;
 		'debian'|'ubuntu')
-			build="${OS[machine]/x86_/amd}"
 			project='https://github.com/dandavison/delta'
+			build="${OS[machine]}"
+			build="${build/aarch/arm}"
+			build="${build/x86_/amd}"
 
-			remote_file '/tmp/delta.deb' \
-				"${project}/releases/download/${version}/git-delta_${version}_${build}.deb" \
-				'sha256:1658c7b61825d411b50734f34016101309e4b6e7f5799944cf8e4ac542cebd7f'
+			case "${build}" in
+				'amd64') checksum='sha256:1658c7b61825d411b50734f34016101309e4b6e7f5799944cf8e4ac542cebd7f' ;;
+				'arm64') checksum='sha256:937781aa7788e1510858743fff6c9a8b4a69fe0a22a7c8a69493e633227939a9' ;;
+				*) error "unexpected: ${build}" ;;
+			esac
+
+			remote_file '/tmp/delta.deb' "${project}/releases/download/${version}/git-delta_${version}_${build}.deb" "${checksum}"
 			sudo dpkg --install '/tmp/delta.deb'
+			rm '/tmp/delta.deb'
 			;;
 		*) error "missing package for ${OS[distribution]}" ;;
 	esac
