c66d46f1c74d2da7fc69e0fe0174f90858163dcb
Chris Bandy <bandy.chris@gmail.com>
2026-03-23 17:10:16 -0500

    Create tmux panes in the directory of the original

--- tmux.sh
+++ tmux.sh
@@ -3,6 +3,7 @@
 
 shopt -s -o errexit nounset
 
-local_file "${HOME}/.tmux.conf" 'files/tmux/tmux.conf'
+mkdir -p "${HOME}/.config/tmux"
+local_file "${HOME}/.config/tmux/tmux.conf" 'files/tmux/tmux.conf'
 
 silent command -v tmux || install_packages 'tmux'
